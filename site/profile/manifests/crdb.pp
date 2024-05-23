45150f86a3b64bc30c43735e3ed6d55a5d7e4603
ayesha.rustgi-delphix <ayesha.rustgi@delphix.com>
2024-05-23 11:59:35 +0000

    CockroachDB env validation

--- site/profile/manifests/crdb.pp
+++ site/profile/manifests/crdb.pp
@@ -1,38 +1,42 @@
-#
-# An example profile for CockroachDB Validations
-#
-
-class profile::crdb{
+class profile::crdb {
   # Define the log file path
   $log_file = '/var/log/crdb_env_log_validation_output.log'
 
   # Check if utilities like netstat, expect are installed on the host
   package { ['netstat', 'expect']:
-    ensure => present,
-    notify => Exec['check_ports'],
+    ensure     => present,
+    notify     => Exec['check_ports'],
+    logoutput  => true,
+    logoutput  => $log_file,
   }
 
   # Check the specified ports using the netstat utility
   exec { 'check_ports':
-    command => "
-      if ! netstat -antp | grep -E '(22|111|2049|1110|54043|54044|54045|26257|26258|26259|8080|8081|8082)'; then
-        echo 'Required ports are not open'
+    command   => "
+      if ! netstat -antp | grep -E '(22|111|2049|1110|54043|54044|54045|26257|26258|26259|8080|8081|8082)' >> $log_file; then
+        echo 'Required ports are not open' >> $log_file
         exit 1
       fi
     ",
-    path    => ['/bin', '/usr/bin'],
-    returns => [0, 1],
+    path      => ['/bin', '/usr/bin'],
+    returns   => [0, 1],
+    logoutput => true,
+    logoutput => $log_file,
   }
 
   # Check if the toolkit directory "/home/delphix/toolkit" is installed
   file { '/home/delphix/toolkit':
-    ensure => 'directory',
+    ensure     => 'directory',
+    logoutput  => true,
+    logoutput  => $log_file,
   }
 
   # Check if the delphix user exists
   exec { 'check_delphix_user':
-    command => "id delphix",
-    path    => ['/bin', '/usr/bin'],
+    command   => "id delphix",
+    path      => ['/bin', '/usr/bin'],
+    logoutput => true,
+    logoutput => $log_file,
   }
 
   # Check if the delphix user has access on the cockroachdb binaries directory
@@ -40,14 +44,18 @@ class profile::crdb{
     command => "ls -ld /u01/cockroach",
     path    => ['/bin', '/usr/bin'],
     onlyif  => "id delphix",
+    logoutput => true,
+    logoutput => $log_file,
   }
 
   # Check if the delphix user has access on the cockroachdb binaries directory
   file { '/u01/cockroach':
-    ensure => 'directory',
-    owner  => 'delphix',
-    group  => 'delphix',
-    mode   => '0755',
+    ensure     => 'directory',
+    owner      => 'delphix',
+    group      => 'delphix',
+    mode       => '0755',
+    logoutput  => true,
+    logoutput  => $log_file,
   }
 
   # Check if cockroachdb binaries are installed, if not install using wget the latest stable version
@@ -61,12 +69,17 @@ class profile::crdb{
     ",
     path    => ['/bin', '/usr/bin', '/u01/cockroach'],
     unless  => "command -v cockroach",
+    logoutput => true,
+    logoutput => $log_file,
   }
 
   # Check the version of installed cockroachdb
   exec { 'check_cockroachdb_version':
-    command => "cockroach version",
-    path    => ['/bin', '/usr/bin', '/u01/cockroach'],
-    onlyif  => "command -v cockroach",
+    command   => "cockroach version",
+    path      => ['/bin', '/usr/bin', '/u01/cockroach'],
+    onlyif    => "command -v cockroach",
+    logoutput => true,
+    logoutput => $log_file,
   }
 }
+
